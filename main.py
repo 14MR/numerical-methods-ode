b4c623bed42454a707839580312008e90360b1d6
Timur <mr14mr@yandex.ru>
2018-11-04 10:10:43 +0300

    ! interfaces of functions

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def f(x, y):
     return y * y * math.exp(x) + 2 * y
 
 
-def reference_solution():
+def reference_solution(x0, y0, x):
     def const_function(x, y):
         # return -(3 * math.exp(2 * x) / y + math.exp(3 * x))
         return 6 * math.exp(2) + math.exp(3)
@@ -26,9 +26,9 @@ def reference_solution():
     def my_function(x, constant):
         return (3 * math.exp(2 * x)) / (constant - math.exp(3 * x))
 
-    const = const_function(INITIAL_X, INITIAL_Y)
+    const = const_function(x0, y0)
 
-    x = [i for i in np.arange(INITIAL_X, ENDING_X + DELTA, DELTA)]  # TODO: посмотреть границы для правой части
+    x = [i for i in np.arange(x0, x + DELTA, DELTA)]  # TODO: посмотреть границы для правой части
     y = []
 
     try:
@@ -40,24 +40,16 @@ def reference_solution():
     return x, y
 
 
-def euler():
+def euler(x0, y0, x):
     def function_y(x):
         if x <= INITIAL_X:
             return INITIAL_Y
         else:
             return function_y(x - DELTA) + DELTA * f(x - DELTA, function_y(x - DELTA))
 
-    x = [i for i in np.arange(INITIAL_X, ENDING_X + DELTA, DELTA)]
+    x = [i for i in np.arange(x0, x + DELTA, DELTA)]
 
-    y = [INITIAL_Y]
-    deleted_x = []
-
-    before_asympt = [i for i in x if i < my_asymp_coordinate - 2 * DELTA]
-    after_asympt = [i for i in x if 4 < i < ENDING_X]
-
-    #
-    # for i in before_asympt:
-    #     print(i, "\n")
+    y = [y0]
 
     for i, v in enumerate(x):
         if i == 0:
@@ -67,27 +59,12 @@ def euler():
             value = float('inf')
         y.insert(i, value)
 
-    # print("\nasymp\n")
-
-    # for i, v in enumerate(after_asympt):
-    #
-    #     if i == 0:
-    #         y.insert(len(y), MIN_Y)
-    #         continue
-    #
-    #     cur_index = len(y)
-    #
-    #     value = y[cur_index - 1] + DELTA * f(after_asympt[i - 1], y[cur_index - 1])
-    #     # print(i, v, value, ' \n')
-    #
-    #     y.insert(cur_index, value)
-
     x, y = x[:len(y)], y
 
     return x, y
 
 
-def runge_kutta():
+def runge_kutta(x0, y0, x):
     def calculate(x, y):
         k1 = DELTA * f(x, y)
         k2 = DELTA * f(x + DELTA / 2, y + k1 / 2)
@@ -96,8 +73,8 @@ def runge_kutta():
 
         return y + (k1 + k2 + k3 + k4) / 6
 
-    x = [i for i in np.arange(INITIAL_X, ENDING_X + DELTA, DELTA)]  # TODO: посмотреть границы для правой части
-    y = [INITIAL_Y]
+    x = [i for i in np.arange(x0, x + DELTA, DELTA)]  # TODO: посмотреть границы для правой части
+    y = [y0]
 
     # try:
     for i, v in enumerate(x):
@@ -117,7 +94,7 @@ def runge_kutta():
     return x, y
 
 
-x, y = reference_solution()
+x, y = reference_solution(INITIAL_X, INITIAL_Y, ENDING_X)
 
 # for i, v in enumerate(x):
 #     print(x[i], y[i], '\n')
@@ -126,13 +103,13 @@ fig, ax = plt.subplots()
 ax.plot(x[:len(y)], y)
 fig.savefig("reference.png")
 
-x, y = euler()
+x, y = euler(INITIAL_X, INITIAL_Y, ENDING_X)
 fig, ax = plt.subplots()
 # ax.axvline(linewidth=my_asymp_coordinate, color='r')
 ax.plot(x, y)
 fig.savefig("euler.png")
 
-x, y = runge_kutta()
+x, y = runge_kutta(INITIAL_X, INITIAL_Y, ENDING_X)
 fig, ax = plt.subplots()
 ax.plot(x, y)
 fig.savefig("runge.png")
