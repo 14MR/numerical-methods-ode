9a377312c357874a6e4381fc39cbbd86062ed407
Timur <mr14mr@yandex.ru>
2018-11-04 23:07:47 +0300

    + global_errors command

--- main.py
+++ main.py
@@ -56,14 +56,40 @@ def plot_graphs(x, y, n):
 
     fig.savefig(f"{directory_for_files}all.png")
 
+    click.echo(f'Generating succeed. Checkout your {directory_for_files} directory.')
+
 
 @cli.command()
-@click.option('--start', default=1, help='Starting number of grid steps')
-@click.option('--end', default=0.5, help='Ending number of grid steps')
-def global_errors(start, end):
+@click.option('--start', default=100, help='Starting number of grid steps')
+@click.option('--end', default=10000, help='Ending number of grid steps')
+@click.option('--step', default=100, help='Size of each step')
+def global_errors(start, end, step):
     """Calculates global error in given range"""
 
-    euler_errors, improved_euler_errors, runge_kutta_errors = ODESolver.investigate_global_errors(int(start), int(end))
+    click.echo(f'Calculating global errors for segment [{start};{end}] with step {step}.')
+
+    euler_errors = []
+    improved_euler_errors = []
+    runge_kutta_errors = []
+
+    r = range(int(start), int(end), int(step))
+
+    with click.progressbar(r) as bar:
+        for i in bar:
+            solver = ODESolver(n=i)
+            solver.calculate_reference()
+
+            solver.calculate_euler()
+            err, max = solver.calculate_euler_errors()
+            euler_errors.append(max)
+
+            solver.calculate_improved_euler()
+            err, max = solver.calculate_improved_euler_errors()
+            improved_euler_errors.append(max)
+
+            solver.calculate_runge_kutta()
+            err, max = solver.calculate_runge_kutta_errors()
+            runge_kutta_errors.append(max)
 
     err_fig, err_ax = plt.subplots()
     err_ax.plot(euler_errors)
